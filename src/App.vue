522714fc64c822a1e1e42b6ab881cedb51f317b5
Djkato <djkatovfx@gmail.com>
2022-08-17 14:28:42 +0200

    finish preview

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-	<input type="button" v-model="useWatcher" @click="useWatcher = !useWatcher" />using: {{ useWatcher ? "Watcher" : "Subscription" }}
-	<ul>
-		<li v-for="(item, index) in myState.objectList" :key="item.name">
-			<IteratedObjectUsingWatch v-if="useWatcher" :itIndex="index"></IteratedObjectUsingWatch>
-			<IteratedObjectUsingSubscribe v-if="!useWatcher" :itIndex="index"></IteratedObjectUsingSubscribe>
-		</li>
-	</ul>
+	<div class="wrapper">
+		<span>
+			<p>Using Pinia Subscription:</p>
+			<ul class="child" v-for="(item, index) in myState.objectList" :key="item.name">
+				<li>
+					<IteratedObjectUsingSubscribe :itIndex="index"></IteratedObjectUsingSubscribe>
+				</li>
+			</ul>
+		</span>
+		<span>
+			<p>Using Vue Watcher:</p>
+			<ul class="child" v-for="(item, index) in myState.objectList" :key="item.name">
+				<li>
+					<IteratedObjectUsingWatch :itIndex="index"></IteratedObjectUsingWatch>
+				</li>
+			</ul>
+		</span>
+	</div>
 </template>
 <script setup>
 import { useMyState } from "./State"
@@ -15,3 +26,13 @@ import IteratedObjectUsingWatch from "./components/iteratedObjectUsingWatch.vue"
 const useWatcher = ref(true)
 const myState = useMyState()
 </script>
+<style>
+.wrapper {
+	display: flex;
+	flex-wrap: wrap;
+}
+.child {
+	display: block;
+	margin: 2vh 15vh;
+}
+</style>
